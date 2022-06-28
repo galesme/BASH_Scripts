cd3b203b6aac06574b362de147043c6adb791a01
EC2 Default User <ec2-user@ip-10-0-8-68.ec2.internal>
2022-06-28 17:21:44 +0000

    Created a script to add a new user

--- user.sh
+++ user.sh
@@ -4,7 +4,7 @@ read user
 if id -u $user > /dev/null 2>&1; then
    echo "This username already taken, please enter a different username"
    else
-    sudo useradd -m $user
+    sudo useradd passwd -d $username
   echo
   echo "The user was successfully added, here is list of all current users: "
    awk -F: '{print $1}' /etc/passwd
